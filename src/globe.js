5782d2d0a7af90af3e6b0b9355764f7b3cea6d12
Amarachukwu <amarachukwuonuohabars007@gmail.com>
2025-07-31 00:02:16 +0100

    finished up

--- src/globe.js
+++ src/globe.js
@@ -3,14 +3,13 @@ import earthTextureURL from  './textures/earth.jpg';
 import bumpMapURL from './textures/earth-bump.jpg'
 import specularMapURL from './textures/earth-specular.jpg';
 import cloudsMapURL from './textures/earth-clouds.jpg';
-
+import stars from './textures/stars.jpg';
 export function initGlobe(container){
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75,container.clientWidth/container.clientHeight, 0.1, 1000);
     const renderer = new THREE.WebGLRenderer({antialias:true});
     renderer.setSize(container.clientWidth, container.clientHeight);
     container.appendChild(renderer.domElement);
-
     const geometry = new THREE.SphereGeometry();
     // const material = new THREE.MeshStandardMaterial({color:0x2194ce, wireframe:true});
     const textureLoader = new THREE.TextureLoader();
@@ -27,6 +26,43 @@ export function initGlobe(container){
     })
     const globe = new THREE.Mesh(geometry, material);
     scene.add(globe);
+
+    const starTexture = new THREE.TextureLoader().load(stars);
+
+        const starGeometry = new THREE.SphereGeometry(90, 64, 64);
+        const starMaterial = new THREE.MeshBasicMaterial({
+        map: starTexture,
+        side: THREE.BackSide,
+    });
+const starfield = new THREE.Mesh(starGeometry, starMaterial);
+scene.add(starfield);
+
+
+   const atmosphereMaterial = new THREE.ShaderMaterial({
+    uniforms: {},
+    vertexShader: `
+        varying vec3 vNormal;
+        void main() {
+        vNormal = normalize(normalMatrix * normal);
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+        }
+    `,
+    fragmentShader: `
+        varying vec3 vNormal;
+        void main() {
+        float intensity = pow(0.9 - dot(vNormal, vec3(0, 0, 1.0)), 3.0);
+        gl_FragColor = vec4(0.3, 0.6, 1.0, 1.0) * intensity;
+        }
+    `,
+    blending: THREE.AdditiveBlending,
+    side: THREE.BackSide,
+    });
+    const atmosphere = new THREE.Mesh(
+    new THREE.SphereGeometry(1.05,64,64),
+    atmosphereMaterial
+    );
+    scene.add(atmosphere);
+
 
     const cloudGeometry = new THREE.SphereGeometry();
     const cloudMaterial = new THREE.MeshLambertMaterial({
@@ -54,6 +90,10 @@ export function initGlobe(container){
     animate();
     return{
         setRotationDelta: (delta)=>{rotationDelta = delta;},
+        adjustZoom:(delta)=>{
+            camera.position.z += delta * 5;
+            camera.position.z = THREE.MathUtils.clamp(camera.position.z, 2,10)
+        }
     }
     
 }
--- src/main.js
+++ src/main.js
@@ -3,27 +3,38 @@ import {initGlobe} from './globe.js';
 import { initHandTracking } from './handTracker.js';
 
 const container = document.getElementById('app');
-const {setRotationDelta} = initGlobe(container);
+const {setRotationDelta,adjustZoom} = initGlobe(container);
 
 let prevX = null;
+let prevPinchDistance = 0;
 
 initHandTracking((results) => {
   if (results.multiHandLandmarks && results.multiHandLandmarks.length > 0) {
     const landmarks = results.multiHandLandmarks[0];
-    const indexTip = landmarks[8]; // Index fingertip
-
+    const indexTip = landmarks[8]; 
+    const thumbTip = landmarks[4];
+    const dx = indexTip.x - thumbTip.x;
+    const dy = indexTip.y - thumbTip.y;
+    const pinchDistance = Math.sqrt(dx*dx+dy*dy);
+    if(prevPinchDistance != null){
+      const delta=pinchDistance-prevPinchDistance;
+      if(Math.abs(delta)>0.005){
+        adjustZoom(delta);
+      }
+    }
     if (prevX !== null) {
       const deltaX = indexTip.x - prevX;
 
       if (Math.abs(deltaX) > 0.01) {
-        setRotationDelta(deltaX * 5); // Adjust rotation sensitivity
+        setRotationDelta(deltaX * 5); 
       } else {
         setRotationDelta(0);
       }
     }
-
+    prevPinchDistance = pinchDistance;
     prevX = indexTip.x;
   } else {
+    prevPinchDistance =null;
     setRotationDelta(0);
     prevX = null;
   }
